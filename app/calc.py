84467758fdccbdd787b83f6dded1ff8437690f84
adri-lavadenz <adri.lavadenz@gmail.com>
2024-06-16 23:53:27 +0000

    fixing some code

--- app/calc.py
+++ app/calc.py
@@ -1,3 +1,10 @@
+import math
+import app
+
+class InvalidPermissions(Exception):
+    pass
+
+
 class Calculator:
     def add(self, x, y):
         self.check_types(x, y)
@@ -8,6 +15,9 @@ class Calculator:
         return x - y
 
     def multiply(self, x, y):
+        if not app.util.validate_permissions(f"{x} * {y}", "user1"):
+            raise InvalidPermissions('User has no permissions')
+
         self.check_types(x, y)
         return x * y
 
@@ -15,16 +25,31 @@ class Calculator:
         self.check_types(x, y)
         if y == 0:
             raise TypeError("Division by zero is not possible")
-
         return x / y
 
     def power(self, x, y):
         self.check_types(x, y)
         return x ** y
 
+    def sqrt(self, x):
+        self.check_single_type(x)
+        if x < 0:
+            raise TypeError("Cannot calculate the square root of a negative number")
+        return math.sqrt(x)
+
+    def log10(self, x):
+        self.check_single_type(x)
+        if x <= 0:
+            raise TypeError("Logarithm base 10 is only defined for positive numbers")
+        return math.log10(x)
+
     def check_types(self, x, y):
         if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
             raise TypeError("Parameters must be numbers")
+    
+    def check_single_type(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Parameter must be a number")
 
 
 if __name__ == "__main__":  # pragma: no cover
